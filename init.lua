dd973b86ce7420661733d04725bb4108dc1cde3c
errorinarrow <kevin30302456@gmail.com>
2025-03-16 19:42:03 +0800

    Add some lsp config

--- init.lua
+++ init.lua
@@ -663,9 +663,22 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
+        cssls = {},
+        html = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = 'strict',
+                autoImportCompletions = true,
+                useLibraryCodeForTypes = true,
+              },
+            },
+          },
+        },
+        ts_ls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -708,6 +721,10 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'pyright',
+        'cssls',
+        'html',
+        'typescript-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,31 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+require('lazy').setup({
+  'neovim/nvim-lspconfig',
+  config = function()
+    local lspconfig = require 'lspconfig'
+    -- Auto complete
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    -- Setting Pyright (Python LSP)
+    lspconfig.pyright.setup {
+      capabilities = capabilities,
+    }
+    -- Setting Lua LSP
+    lspconfig.lua_ls.setup {
+      settings = {
+        lua = {
+          diagnostics = { globals = { 'vim' } },
+        },
+      },
+    }
+  end,
+}, {
+  'williamboman/mason.nvim',
+  config = function()
+    require('mason').setup()
+    require('mason-lspconfig').setup {
+      ensure_installed = { 'pyright', 'lua_ls' },
+    }
+  end,
+})
